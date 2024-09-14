ee7c58e01581953c97233f11ecaf3c9a2b0287c1
chenzehhhh <14076261+chenzehhhhh@user.noreply.gitee.com>
2024-09-14 14:26:11 +0800

    项目测试

---
+++ src/test/java/com/czhhhh/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czhhhh.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
